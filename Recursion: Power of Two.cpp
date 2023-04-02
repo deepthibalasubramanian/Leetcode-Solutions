9eab1872e6a95ac7e61304a840e755dd3b398085
Deepthi B <91285032+deepthibalasubramanian@users.noreply.github.com>
2023-04-02 19:18:34 +0530

    Create Recursion: Power of Two.cpp

---
+++ Recursion: Power of Two.cpp
@@ -0,0 +1,17 @@
+/*Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.*/
+
+
+
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+            if ((n<=0) || (n==1))
+            return true;
+            else if (n%2==0){
+            return isPowerOfTwo(n/2);}
+            return false;
+    }
+};
