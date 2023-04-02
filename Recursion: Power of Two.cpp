09d99446cc3976b8710d6f5ee7c0f2a8bb667f22
Deepthi B <91285032+deepthibalasubramanian@users.noreply.github.com>
2023-04-02 19:20:06 +0530

    Update Recursion: Power of Two.cpp

--- Recursion: Power of Two.cpp
+++ Recursion: Power of Two.cpp
@@ -2,16 +2,16 @@
 
 An integer n is a power of two, if there exists an integer x such that n == 2x.*/
 
-
-
-
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-            if ((n<=0) || (n==1))
+            if (n<=0)
+            return false;
+            else if (n==1)
             return true;
             else if (n%2==0){
             return isPowerOfTwo(n/2);}
+            else
             return false;
     }
 };
